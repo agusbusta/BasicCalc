29f77c2d0a5897f9a6fadad2718e840741adb2e7
agusbusta <agusbus95@gmail.com>
2022-10-30 00:37:03 -0300

    js commented

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 let result = document.getElementById("inputext");
 
-
+//function to display entries.
 let calculate=(number)=>{
     result.value+=number;
 }
 
-
+//funcion to invalidate no valid entries. 
 let Result=()=>{
     try{
         result.value=eval(result.value);
@@ -14,15 +14,16 @@ let Result=()=>{
         alert("Enter a valid entry");
     }
 }
-
+//funcion to clear display
 function clr(){
     result.value = " "; 
 }
-
+//function to erase one entry
 function del(){
     result.value=result.value.slice(0,-1);
 }
 
+//function to make a percentage operation. 
 function perc(){
     result.value=result.value / 100;
 }
